72d3a07c1417b83dade6c38df59289fcd98f6328
dantnc <fad11204@yahoo.com.tw>
2018-06-28 23:12:06 +0800

    [Chore] init of each module

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
 import Process as P
 import UI
 import ImageIO as IIO
+
+class Main(object):
+	def __init__():
+		self.ui = UI.UI()
+		self.iio = IIO.ImageIO("test.png", "test_o.png")
+
+	def run():
+		pass
+
+if __name__ == '__main__':
+	Main().run()
