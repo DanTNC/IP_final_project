2047b25bad3e746979e8cbe5fe91ec7b966013c7
dantnc <fad11204@yahoo.com.tw>
2018-07-02 22:15:32 +0800

    [Feature] build structure of Main.py and UI.py

--- Main.py
+++ Main.py
@@ -4,10 +4,28 @@ import ImageIO as IIO
 
 class Main(object):
 	def __init__():
-		self.ui = UI.UI()
+		self.ui = UI.UI(self)
 		self.iio = IIO.ImageIO("test.png", "test_o.png")
+		self.image = None
+		self.result = None
 
-	def run():
+	def run(self):
+		self.ui.main_menu()
+
+	def import_image(self, name):
+		self.image = self.iio.get_input_image(name, "L")
+		self.result = None
+		return self.image
+
+	def process(self, borders):
+		self.result = P.run(self.image, *borders)
+		return self.result
+
+	def save_image(self, name):
+		self.iio.save_output_image(name)
+
+	def overlap(self):
+		# overlap image and result
 		pass
 
 if __name__ == '__main__':
--- UI.py
+++ UI.py
@@ -1,2 +1,39 @@
+import Tkinter as TK
+import tkFileDialog as TKFD
+
 class UI(object):
-	pass
+	def __init__(self, main):
+		self.main = main
+		self.window = TK.TK()
+		self.x = self.window.winfo_screenwidth()
+		self.y = self.window.winfo_screenheight()
+		self.place(self.window)
+
+	def place(self, window):
+		window.geometry("+{}+{}".format(int(self.x/2.5), int(self.y/2.5)))
+
+	def main_menu(self):
+		self.window.title("heart image processer")
+
+		# TK.Button()
+
+	def import_image(self):
+		img = self.TKFD.askopenfilenames(initialdir="img/",
+										filetypes=[("images",".jpg .png")],
+										title="Please choose images to import",
+										parent=self.window)
+		self.imagename = img
+		self.imshow(self.main.import_image(img))
+
+	def process(self):
+		self.main.process(self.borders())
+		self.imshow(self.main.overlap())
+
+	def save(self):
+		fn, ext = self.imagename.split(".")
+		self.main.save_image("{}_o.{}".format(fn, ext))
+
+	def move_border(self):
+		# get button name
+		# modify border textvar
+		pass
