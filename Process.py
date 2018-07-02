a9d714e65578e116a3c100eeb06f3eaa16d9c023
dantnc <fad11204@yahoo.com.tw>
2018-07-02 15:31:50 +0800

    [Chore] init Process.py

--- Process.py
+++ Process.py
@@ -1,4 +1,10 @@
 from IP_funcs import *
+import ImageIO as IIO
 
 def run(arr, x1, x2, y1, y2):
 	return res
+
+if __name__=="__main__":
+	iio = IIO.ImageIO()
+	arr = iio.get_input_image("img/I0071695_1.jpg", "L")
+	run(arr, )
